fca55a3048221c90a4a3a3ccdefd6f143e52ad99
Steven Op de beeck <steven@opdebeeck.org>
2021-12-02 21:19:10 +0100

    Day 2: restructure

--- src/main/kotlin/Day02Pilot.kt
+++ src/main/kotlin/Day02Pilot.kt
@@ -1,9 +1,35 @@
 import Command.Keyword.valueOf
 
+fun main() {
+    val input = readInput("Day02").map {
+        val split = it.split(' ') //^(\w+)\s(\d+)$
+        Command(valueOf(split[0].uppercase()), split[1].toInt())
+    }
+
+    val pos1 = partOne(input)
+    println("Part1: $pos1 == ${pos1.depth * pos1.horizontal}")
+
+    val pos2 = partTwo(input)
+    println("Part2: $pos2 == ${pos2.depth * pos2.horizontal}")
+}
+
+private fun partOne(input: List<Command>): Position {
+    var position = Position()
+    input.forEach { position = it.move(position) }
+    return position
+}
+
+private fun partTwo(input: List<Command>): Position {
+    var position: Position = AimPosition()
+    input.forEach { position = it.move(position) }
+    return position
+}
+
 open class Position(val horizontal: Int = 0, val depth: Int = 0) {
     open fun forward(units: Int): Position = Position(horizontal + units, depth)
     open fun down(units: Int): Position = Position(horizontal, depth + units)
     open fun up(units: Int): Position = down(-units)
+    override fun toString(): String = "Position(horizontal=$horizontal, depth=$depth)"
 }
 
 class AimPosition(horizontal: Int = 0, depth: Int = 0, val aim: Int = 0) : Position(horizontal, depth) {
@@ -12,33 +38,11 @@ class AimPosition(horizontal: Int = 0, depth: Int = 0, val aim: Int = 0) : Posit
     override fun up(units: Int): AimPosition = down(-units)
 }
 
-
 class Command(private val keyword: Keyword, private val units: Int) {
+
     enum class Keyword(val operation: (Position, Int) -> Position) {
         FORWARD(Position::forward), UP(Position::up), DOWN(Position::down);
     }
 
     fun move(position: Position): Position = this.keyword.operation.invoke(position, this.units)
-}
-
-private fun partOne(input: List<Command>) {
-    var position = Position()
-    input.forEach { position = it.move(position) }
-    println("Part1: $position == ${position.depth * position.horizontal}")
-}
-
-private fun partTwo(input: List<Command>) {
-    var position: Position = AimPosition()
-    input.forEach { position = it.move(position) }
-    println("Part2: $position == ${position.depth * position.horizontal}")
-}
-
-fun main() {
-    val input = readInput("Day02").map {
-        val split = it.split(' ') //^(\w+)\s(\d+)$
-        Command(valueOf(split[0].uppercase()), split[1].toInt())
-    }
-
-    partOne(input)
-    partTwo(input)
 }
