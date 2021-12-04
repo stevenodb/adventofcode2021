0865b5f1a7d51ccaa684687eaeb0e4f2cbfc7ed6
Steven Op de beeck <steven@opdebeeck.org>
2021-12-04 13:20:33 +0100

    Day3 part 2

--- src/main/kotlin/Day03Diagnostic.kt
+++ src/main/kotlin/Day03Diagnostic.kt
@@ -2,10 +2,27 @@ fun main() {
     val input = readInput("Day03")
 
     val bitResult = reduceToCommonBits(input)
-    val result = bitResult.joinToString("").toInt(2)
-    val invResult = bitResult.map { if (it == 0) 1 else 0 }.joinToString("").toInt(2)
+    val gamma = bitResult.joinToString("").toInt(2)
+    val epsilon = bitResult.map { if (it == 0) 1 else 0 }.joinToString("").toInt(2)
 
-    println("$result * $invResult = ${result * invResult}")
+    println("Part 1: $gamma * $epsilon = ${gamma * epsilon}")
+
+    val oxygen = reduceToValueByBitCommonality(input).toInt(2)
+    val co2 = reduceToValueByBitCommonality(input, false).toInt(2)
+    println("Part 2: $oxygen * $co2 = ${oxygen * co2}")
+
+}
+
+fun reduceToValueByBitCommonality(input: List<String>, mostCommon: Boolean = true): String {
+    val bitCount = input.first().length
+    var bitNumbers = input
+    for (bit in 0 until bitCount) {
+        val count = bitNumbers.map { it[bit] }.count { it == '1' }
+        val selectedBit = if (!mostCommon xor (count >= bitNumbers.size / 2.0)) '1' else '0'
+        bitNumbers = bitNumbers.filter { it[bit] == selectedBit }
+        if (bitNumbers.size == 1) break
+    }
+    return bitNumbers.single()
 }
 
 fun reduceToCommonBits(bitNumbers: List<String>): List<Int> {
--- src/test/kotlin/Day03DiagnosticKtTest.kt
+++ src/test/kotlin/Day03DiagnosticKtTest.kt
@@ -4,23 +4,36 @@ import strikt.assertions.isEqualTo
 
 internal class Day03DiagnosticKtTest {
 
+    private var input = listOf(
+        "00100",
+        "11110",
+        "10110",
+        "10111",
+        "10101",
+        "01111",
+        "00111",
+        "11100",
+        "10000",
+        "11001",
+        "00010",
+        "01010"
+    )
+
     @Test
     internal fun reduceToCommonBits_scenario() {
-        val input = listOf(
-            "00100",
-            "11110",
-            "10110",
-            "10111",
-            "10101",
-            "01111",
-            "00111",
-            "11100",
-            "10000",
-            "11001",
-            "00010",
-            "01010"
-        )
         val actual = reduceToCommonBits(input)
-        expectThat(actual).isEqualTo(listOf(1,0,1,1,0))
+        expectThat(actual).isEqualTo(listOf(1, 0, 1, 1, 0))
+    }
+
+    @Test
+    internal fun reduceToValueByBitCommonality_scenario_mostCommon() {
+        val actual = reduceToValueByBitCommonality(input, mostCommon = true)
+        expectThat(actual).isEqualTo("10111")
+    }
+
+    @Test
+    internal fun reduceToValueByBitCommonality_scenario_leastCommon() {
+        val actual = reduceToValueByBitCommonality(input, mostCommon = false)
+        expectThat(actual).isEqualTo("01010")
     }
 }
