06eeec9f51199f8b37e5ddbf0ce18e36c780cba2
Steven Op de beeck <steven@opdebeeck.org>
2021-12-04 10:18:49 +0100

    Day3 part 1

---
+++ src/main/kotlin/Day03Diagnostic.kt
@@ -0,0 +1,28 @@
+import kotlin.math.pow
+
+fun main() {
+    val input = readInput("Day03")
+    val numberOfBits = input.first().length
+
+    val bitResult = reduceToCommonBits(numberOfBits, input.map { it.toInt(2) })
+    val result = bitResult.bitwiseToInteger()
+    val invResult = bitResult.map { if (it == 0) 1 else 0 }.bitwiseToInteger()
+
+    println("$result * $invResult = ${result * invResult}")
+}
+
+internal fun reduceToCommonBits(numberOfBits: Int, ints: List<Int>): List<Int> {
+    val bitCount = Array(numberOfBits) { 0 }
+    for (i in 0 until numberOfBits) {
+        val pow = 2 `**` i
+        for (int in ints) {
+            bitCount[i] += (int and pow) / pow
+        }
+    }
+    return bitCount.map { if (it >= ints.size / 2) 1 else 0 }.reversed()
+}
+
+private fun List<Int>.bitwiseToInteger() =
+    this.reversed().reduceIndexed { index, acc, int -> println(index); acc or (int * (2 `**` index)) }
+
+private infix fun Int.`**`(i: Int) = this.toDouble().pow(i).toInt()
---
+++ src/test/kotlin/Day03DiagnosticKtTest.kt
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test
+import strikt.api.expectThat
+import strikt.assertions.isEqualTo
+
+internal class Day03DiagnosticKtTest {
+
+    @Test
+    internal fun reduceToCommonBits_scenario() {
+        val input = listOf(
+            "00100",
+            "11110",
+            "10110",
+            "10111",
+            "10101",
+            "01111",
+            "00111",
+            "11100",
+            "10000",
+            "11001",
+            "00010",
+            "01010"
+        )
+        val actual = reduceToCommonBits(5, input.map { it.toInt(2) })
+        expectThat(actual).isEqualTo(listOf(1,0,1,1,0))
+    }
+}
