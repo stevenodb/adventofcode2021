a1bd577edf838c6ab082b991778af0edfdcfaa7c
Steven Op de beeck <steven@opdebeeck.org>
2021-12-06 10:24:11 +0100

    Day 6 part 2

--- src/main/kotlin/Day6Woohoo.kt
+++ src/main/kotlin/Day6Woohoo.kt
@@ -1,19 +1,26 @@
 fun main() {
-    val fishTimers = readInput("Day06").flatMap { it.split(",") }.map { it.toInt() }.toMutableList()
+    val fishCounts = readInput("Day06")
+        .flatMap { it.split(",") }
+        .map { it.toInt() }
+        .groupingBy { it }
+        .aggregate { _, accumulator: Long?, _, first -> if (first) 1 else accumulator!! + 1 }.toMutableMap()
 
-    val numDays = 80
+    val count80 = fishWoohoo(80, fishCounts)
+    println("Part 1: Fish after 80: $count80")
+    val count256 = fishWoohoo(256, fishCounts)
+    println("Part 2: Fish after 256: $count256")
+}
+
+fun fishWoohoo(numDays: Int, fishCountsInput: MutableMap<Int, Long>): Long {
+    val fishCounts = HashMap(fishCountsInput)
     for (day in 1..numDays) {
-        var idx = 0
-        val numberThisDay = fishTimers.size
-        while (idx < numberThisDay) {
-            if (fishTimers[idx] == 0) {
-                    fishTimers[idx] = 6
-                    fishTimers.add(8)
-                } else {
-                fishTimers[idx]--
-            }
-            idx++
+        val zeroFishCount = fishCounts[0] ?: 0
+        for (key in 1..8) {
+            fishCounts[key - 1] = fishCounts[key] ?: 0
         }
+        fishCounts.remove(8)
+        fishCounts[6] = (fishCounts[6] ?: 0) + zeroFishCount
+        fishCounts[8] = (fishCounts[8] ?: 0) + zeroFishCount
     }
-    println("Part 1: Fish after $numDays: ${fishTimers.size}")
+    return fishCounts.values.sum()
 }
---
+++ src/test/kotlin/Day6WoohooKtTest.kt
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test
+import strikt.api.expectThat
+import strikt.assertions.isEqualTo
+
+internal class Day6WoohooKtTest {
+
+    @Test
+    internal fun fishWoohoo_scenario() {
+        val actual = fishWoohoo(80, hashMapOf(4 to 1, 3 to 2, 1 to 1, 2 to 1))
+        expectThat(actual).isEqualTo(5934)
+    }
+}
