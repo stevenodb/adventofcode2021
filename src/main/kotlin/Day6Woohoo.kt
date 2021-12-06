deba8b4ea5c0bb70b2db3eb4178aa31cfb7a5f96
Steven Op de beeck <steven@opdebeeck.org>
2021-12-06 11:36:57 +0100

    clean up code

--- src/main/kotlin/Day6Woohoo.kt
+++ src/main/kotlin/Day6Woohoo.kt
@@ -3,24 +3,25 @@ fun main() {
         .flatMap { it.split(",") }
         .map { it.toInt() }
         .groupingBy { it }
-        .aggregate { _, accumulator: Long?, _, first -> if (first) 1 else accumulator!! + 1 }.toMutableMap()
-
+        .eachCount()
     val count80 = fishWoohoo(80, fishCounts)
     println("Part 1: Fish after 80: $count80")
     val count256 = fishWoohoo(256, fishCounts)
     println("Part 2: Fish after 256: $count256")
 }
 
-fun fishWoohoo(numDays: Int, fishCountsInput: MutableMap<Int, Long>): Long {
-    val fishCounts = HashMap(fishCountsInput)
+fun fishWoohoo(numDays: Int, fishCountsInput: Map<Int, Int>): ULong {
+    val fishCounts = mutableMapOf<Int, ULong>()
+    fishCountsInput.entries.associateTo(fishCounts) { (key, value) -> Pair(key, value.toULong()) }
+
     for (day in 1..numDays) {
-        val zeroFishCount = fishCounts[0] ?: 0
+        val zeroFishCount = fishCounts[0] ?: 0uL
         for (key in 1..8) {
-            fishCounts[key - 1] = fishCounts[key] ?: 0
+            fishCounts[key - 1] = fishCounts[key] ?: 0uL
         }
         fishCounts.remove(8)
-        fishCounts[6] = (fishCounts[6] ?: 0) + zeroFishCount
-        fishCounts[8] = (fishCounts[8] ?: 0) + zeroFishCount
+        fishCounts[6] = (fishCounts[6] ?: 0uL) + zeroFishCount
+        fishCounts[8] = (fishCounts[8] ?: 0uL) + zeroFishCount
     }
     return fishCounts.values.sum()
 }
--- src/test/kotlin/Day6WoohooKtTest.kt
+++ src/test/kotlin/Day6WoohooKtTest.kt
@@ -7,6 +7,6 @@ internal class Day6WoohooKtTest {
     @Test
     internal fun fishWoohoo_scenario() {
         val actual = fishWoohoo(80, hashMapOf(4 to 1, 3 to 2, 1 to 1, 2 to 1))
-        expectThat(actual).isEqualTo(5934)
+        expectThat(actual).isEqualTo(5934uL)
     }
 }
