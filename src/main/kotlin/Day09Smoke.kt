b3890fbd644c9eef269c63eccda2ebe349838e9a
Steven Op de beeck <steven@opdebeeck.org>
2021-12-10 08:43:20 +0100

    Day 9 part 2

--- src/main/kotlin/Day09Smoke.kt
+++ src/main/kotlin/Day09Smoke.kt
@@ -1,17 +1,55 @@
 fun main() {
     val heightMap = readInput("Day09").map { line -> line.toList().map { char -> char.digitToInt() } }
-    println(determineRiskLevels(heightMap))
+    val lowPoints = findLowPoints(heightMap)
+
+    println("Part 1: ${determineRiskLevels(lowPoints.map { (_, height) -> height })}")
+    val largestBasins = determineBasinsProduct(heightMap, lowPoints.map { (point, _) -> point })
+    println("Part 2: ${largestBasins}")
 }
 
-fun determineRiskLevels(heightMap: List<List<Int>>): Int {
-    return findLowPoints(heightMap).sumOf { it + 1 }
+fun determineBasinsProduct(heightMap: List<List<Int>>, lowPoints: List<Pair<Int, Int>>): Int {
+    val visitMap = heightMap.map { it.toMutableList() }.toList()
+    val result = lowPoints.map { (x, y) -> exploreBasin(visitMap, x, y) + 1 }
+    println(visitMap.joinToString("") { it.joinToString("", postfix = "\n") })
+    return result.sortedDescending().take(3).reduce { acc, int -> acc * int }
 }
 
-private fun findLowPoints(heightMap: List<List<Int>>) =
-    heightMap.flatMapIndexed { y, row -> row.filterIndexed { x, height -> heightMap.isLowPoint(x, y, height) } }
+private fun exploreBasin(visitMap: List<MutableList<Int>>, x: Int, y: Int): Int {
+    visitMap[y][x] = 0
+    return (-1..1).flatMap { iy ->
+        (-1..1)
+            .asSequence()
+            .filterNot { ix -> (ix != 0) && (iy != 0) }
+            .filterNot { ix -> (ix == 0) && (iy == 0) }
+            .map { ix -> (x + ix) to (y + iy) }
+            .filter { (nx, ny) ->
+                visitMap.getOrNull(ny)?.getOrNull(nx)?.let { height ->
+                    when (height) {
+                        0 -> false
+                        9 -> false
+                        else -> true
+                    }
+                } == true
+            }
+            .map { (nx, ny) ->
+                exploreBasin(visitMap, nx, ny) + 1
+            }
+    }.sum()
+}
+
+internal fun determineRiskLevels(lowPoints: List<Int>): Int {
+    return lowPoints.sumOf { it + 1 }
+}
+
+internal fun findLowPoints(heightMap: List<List<Int>>) =
+    heightMap.flatMapIndexed { y, row ->
+        row.mapIndexed { x, height -> (x to y) to heightMap.isLowPoint(x, y, height) }
+            .filter { (_, isLowPoint) -> isLowPoint }
+            .map { (point, _) -> point to heightMap[point.second][point.first] }
+    }
 
 private fun <T : Comparable<T>> List<List<T>>.isLowPoint(x: Int, y: Int, currentHeight: T): Boolean {
-    var adjacentCount = 0
+    var maxAdjacent = 0
     val adjacentHeights = ArrayList<T>()
     (-1..1).forEach { iy ->
         (-1..1)
@@ -23,9 +61,9 @@ private fun <T : Comparable<T>> List<List<T>>.isLowPoint(x: Int, y: Int, current
                         if (currentHeight < adjacentHeight) {
                             adjacentHeights.add(adjacentHeight)
                         }
-                        adjacentCount++
+                        maxAdjacent++
                     }
             }
     }
-    return adjacentHeights.size == adjacentCount
+    return adjacentHeights.size == maxAdjacent
 }
--- src/test/kotlin/Day09SmokeKtTest.kt
+++ src/test/kotlin/Day09SmokeKtTest.kt
@@ -12,7 +12,24 @@ internal class Day09SmokeKtTest {
             listOf(8, 7, 6, 7, 8, 9, 6, 7, 8, 9),
             listOf(9, 8, 9, 9, 9, 6, 5, 6, 7, 8)
         )
-        val actual = determineRiskLevels(heightMap)
+        val lowPoints = findLowPoints(heightMap)
+
+        val actual = determineRiskLevels(lowPoints.map { (_, height) -> height })
         expectThat(actual).isEqualTo(15)
+    }
+
+    @Test
+    fun findBasins_scenario() {
+        val heightMap = listOf(
+            listOf(2, 1, 9, 9, 9, 4, 3, 2, 1, 0),
+            listOf(3, 9, 8, 7, 8, 9, 4, 9, 2, 1),
+            listOf(9, 8, 5, 6, 7, 8, 9, 8, 9, 2),
+            listOf(8, 7, 6, 7, 8, 9, 6, 7, 8, 9),
+            listOf(9, 8, 9, 9, 9, 6, 5, 6, 7, 8)
+        )
+        val lowPoints = findLowPoints(heightMap)
+
+        val actual = determineBasinsProduct(heightMap, lowPoints.map { (point, _) -> point })
+        expectThat(actual).isEqualTo(1134)
     }
 }
