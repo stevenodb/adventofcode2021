d4a71c55b8e258cb8fb5e705ffbff7ecfb335e91
Steven Op de beeck <steven@opdebeeck.org>
2021-12-11 17:02:19 +0100

    Introduce OctoField class

--- src/main/kotlin/Day11Octopussy.kt
+++ src/main/kotlin/Day11Octopussy.kt
@@ -5,84 +5,92 @@ fun main() {
 }
 
 fun countFlashes(inputLines: List<String>): Int {
-    val octoField = parseInput(inputLines)
+    val octoField = OctoField(inputLines)
     var flashCount = 0
     repeat(100) {
         val toHandleFlashes = mutableListOf<Point>()
 
-        toHandleFlashes.addAll(traverseField(octoField) { octo, _ ->
+        toHandleFlashes.addAll(octoField.traverseField { octo, _ ->
             return@traverseField octo.increaseEnergy()
         })
         flashCount += toHandleFlashes.size
 
         while (toHandleFlashes.isNotEmpty()) {
             val flashPoint = toHandleFlashes.removeFirst()
-            val secondaryFlashes = handleFlash(octoField, flashPoint)
+            val secondaryFlashes = octoField.handleFlash(flashPoint)
             flashCount += secondaryFlashes.size
             secondaryFlashes.forEach { toHandleFlashes.add(it) }
         }
     }
-    println(octoField.joinToString("") { it.joinToString("", postfix = "\n") })
     return flashCount
 }
 
-private fun parseInput(inputLines: List<String>): List<List<Octo>> {
-    val octoField = inputLines.map { line ->
-        line.map { char -> char.digitToInt() }.map { Octo(it) }
-    }
-    return octoField
-}
+class OctoField(lines: List<String>) {
+    private val octoField: List<List<Octo>> = parseInput(lines)
 
-private fun traverseField(octoField: List<List<Octo>>, operation: (Octo, Point) -> Boolean): List<Point> {
-    val points = mutableListOf<Point>()
-    octoField.forEachIndexed { y, row ->
-        row.forEachIndexed { x, octo ->
-            val point = Point(x, y)
-            val b = operation(octo, point)
-            if (b) points.add(point)
+    private fun parseInput(inputLines: List<String>): List<List<Octo>> {
+        return inputLines.map { line ->
+            line.map { char -> char.digitToInt() }
+                .map { Octo(it) }
         }
     }
-    return points
-}
 
-private fun handleFlash(octoField: List<List<Octo>>, point: Point): List<Point> {
-    val flashPoints = mutableListOf<Point>()
-    with(point) {
-        (-1..1).map { it + y }.forEach { ny ->
-            (-1..1).map { it + x }.forEach { nx ->
-                val didFlash = when {
-                    nx == x && ny == y -> false
-                    ny !in octoField.indices -> false
-                    nx !in octoField[0].indices -> false
-                    octoField[ny][nx].hasFlashed() -> false
-                    else -> octoField[ny][nx].increaseEnergy()
-                }
-                if (didFlash) flashPoints.add(Point(nx, ny))
+    fun traverseField(operation: (Octo, Point) -> Boolean): List<Point> {
+        val points = mutableListOf<Point>()
+        octoField.forEachIndexed { y, row ->
+            row.forEachIndexed { x, octo ->
+                val point = Point(x, y)
+                val b = operation(octo, point)
+                if (b) points.add(point)
             }
         }
+        return points
     }
-    return flashPoints
-}
 
-internal data class Point(val x: Int, val y: Int)
-
-internal data class Octo(private var energy: Int) {
-
-    fun increaseEnergy(): Boolean {
-        if (++energy > 9) {
-            return flash()
+    fun handleFlash(point: Point): List<Point> {
+        val flashPoints = mutableListOf<Point>()
+        with(point) {
+            (-1..1).map { it + y }.forEach { ny ->
+                (-1..1).map { it + x }.forEach { nx ->
+                    val didFlash = when {
+                        nx == x && ny == y -> false
+                        ny !in octoField.indices -> false
+                        nx !in octoField[0].indices -> false
+                        octoField[ny][nx].hasFlashed() -> false
+                        else -> octoField[ny][nx].increaseEnergy()
+                    }
+                    if (didFlash) flashPoints.add(Point(nx, ny))
+                }
+            }
         }
-        return false
-    }
-
-    fun hasFlashed(): Boolean = energy == 0
-
-    private fun flash(): Boolean {
-        energy = 0
-        return true
+        return flashPoints
     }
 
     override fun toString(): String {
-        return energy.toString()
+        return octoField.joinToString("") { it.joinToString("", postfix = "\n") }
+    }
+
+
+    data class Octo(private var energy: Int) {
+
+        fun increaseEnergy(): Boolean {
+            if (++energy > 9) {
+                return flash()
+            }
+            return false
+        }
+
+        fun hasFlashed(): Boolean = energy == 0
+
+        private fun flash(): Boolean {
+            energy = 0
+            return true
+        }
+
+        override fun toString(): String {
+            return energy.toString()
+        }
     }
 }
+
+data class Point(val x: Int, val y: Int)
