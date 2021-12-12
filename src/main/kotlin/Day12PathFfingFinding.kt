65c8f47c5fec69ee3403138b4f9efac198b8dafc
Steven Op de beeck <steven@opdebeeck.org>
2021-12-12 14:53:41 +0100

    Day 12 Part1

---
+++ src/main/kotlin/Day12PathFfingFinding.kt
@@ -0,0 +1,37 @@
+fun main() {
+    val input = parseInput(readInput("Day12"))
+    val allPaths = findAllPaths(input, "start")
+//    printPaths(allPaths)
+    println("Part 1: ${allPaths.size}")
+}
+
+internal fun printPaths(paths: List<List<String>>) {
+    println(paths.map { path -> path.joinToString(",", postfix = "\n") }.sortedBy { it.length }.joinToString(""))
+}
+
+internal fun parseInput(strings: List<String>) =
+    strings.map { line -> line.split('-') }
+        .flatMap { listOf(it.toFirstLastPair(), it.toFirstLastPair().reversed()) }
+        .groupBy({ it.first }, { it.second })
+
+fun findAllPaths(
+    connections: Map<String, List<String>>, node: String, path: List<String> = listOf()
+): MutableList<List<String>> {
+    if (node == "end") return mutableListOf(path.plus(node))
+    val nextNodes = connections[node] ?: return mutableListOf()
+
+    val newPath = path.plus(node)
+    val foundPaths = mutableListOf<List<String>>()
+    nextNodes.forEach { nextNode ->
+        if (!(nextNode.isLowerCase() && nextNode in path)) {
+            foundPaths.addAll(findAllPaths(connections, nextNode, newPath))
+        }
+    }
+    return foundPaths
+}
+
+private fun String.isLowerCase() = all { char -> char.isLowerCase() }
+
+private fun <T> Iterable<T>.toFirstLastPair(): Pair<T, T> = Pair(first(), last())
+
+private fun <A, B> Pair<A, B>.reversed(): Pair<B, A> = Pair(second, first)
---
+++ src/test/kotlin/Day12PathFfingFindingKtTest.kt
@@ -0,0 +1,55 @@
+import org.junit.jupiter.api.Test
+import strikt.api.expectThat
+import strikt.assertions.isEqualTo
+
+internal class Day12PathFfingFindingKtTest {
+    @Test
+    fun findAllPaths_scenario19() {
+        val input = listOf(
+            "dc-end",
+            "HN-start",
+            "start-kj",
+            "dc-start",
+            "dc-HN",
+            "LN-dc",
+            "HN-end",
+            "kj-sa",
+            "kj-HN",
+            "kj-dc"
+        )
+
+        val connectionMap = parseInput(input)
+        val allPaths = findAllPaths(connectionMap, "start")
+        expectThat(allPaths.size).isEqualTo(19)
+        printPaths(allPaths)
+    }
+
+    @Test
+    fun findAllPaths_scenario226() {
+        val input = listOf(
+            "fs-end",
+            "he-DX",
+            "fs-he",
+            "start-DX",
+            "pj-DX",
+            "end-zg",
+            "zg-sl",
+            "zg-pj",
+            "pj-he",
+            "RW-he",
+            "fs-DX",
+            "pj-RW",
+            "zg-RW",
+            "start-pj",
+            "he-WI",
+            "zg-he",
+            "pj-fs",
+            "start-RW",
+        )
+
+        val connectionMap = parseInput(input)
+        val allPaths = findAllPaths(connectionMap, "start")
+        expectThat(allPaths.size).isEqualTo(226)
+        printPaths(allPaths)
+    }
+}
